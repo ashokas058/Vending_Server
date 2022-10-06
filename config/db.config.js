6c0ba676c61d06dfa374f214dbcda8bd13717850
ashokas058 <ashokas058@gmail.com>
2022-10-06 08:38:27 -0700

    pg-update

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,10 @@
-const { createPool } = require("mysql");
-const db = createPool({
-  port: 5432,
-  host: "localhost",
-  user: "hbrtsbwkhdgqfy",
-  password: "518ced4f708135b0812fced4bbd93b672a7970b534746cdffe029f14ced65414",
-  database: "duishf25hku4v",
-  
+const { Client } = require('pg');
+
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = db;
+module.exports=db;
