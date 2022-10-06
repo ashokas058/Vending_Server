cf9a724c303b2daba24e348fbe819ddd6880cdca
ashokas058 <ashokas058@gmail.com>
2022-10-06 08:13:43 -0700

    db-local-heruk

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const { createPool } = require("mysql");
 const db = createPool({
   port: 5432,
-  host: "ec2-34-200-205-45.compute-1.amazonaws.com",
+  host: "localhost",
   user: "hbrtsbwkhdgqfy",
   password: "518ced4f708135b0812fced4bbd93b672a7970b534746cdffe029f14ced65414",
   database: "duishf25hku4v",
